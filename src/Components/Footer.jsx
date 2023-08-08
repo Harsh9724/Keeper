eb971ac89cf0f86c993ec9efa1487e7eda71515e
Harsh9724 <Harsh972438@gmail.com>
2023-08-08 22:51:00 +0530

    deleted:    src/App.js 	modified:   src/Components/Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return (
-        <footer><p>© 2020 Keeper App. All Rights Reserved.</p></footer>
+        <footer><p>© 2023 Keeper App. All Rights Reserved.</p></footer>
     );
 }
 
